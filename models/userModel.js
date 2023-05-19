ed4b9f102b17e805540e8c40b3c19446a2e77bd4
mdsoliamn-io <developer.mdsoliman@gmail.com>
2023-05-19 02:02:22 -0700

    user password hash use bcrypt

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose"); // Erase if already required
-
+const bcrypt = require("bcrypt");
 // Declare the Schema of the Mongo model
 var userSchema = new mongoose.Schema({
   firstname: {
@@ -25,6 +25,9 @@ var userSchema = new mongoose.Schema({
     required: true,
   },
 });
-
+userSchema.pre("save", async function () {
+  const salt = await bcrypt.genSaltSync(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
 //Export the model
 module.exports = mongoose.model("user", userSchema);
